d8290d356346ceac2df3a26dc0c9f6632727faca
Madhav Chintha <mkchintha22@gmail.com>
2025-07-15 22:04:47 +0530

    intial commit

---
+++ src/test/java/org/mysolutions/resell/products/ResellProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mysolutions.resell.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
